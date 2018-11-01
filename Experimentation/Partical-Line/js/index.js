0732e53745bfd690ec5cb48bf4b34afdbbb5a23d
ThomasKerslake <33869524+ThomasKerslake@users.noreply.github.com>
2018-11-01 12:05:06 +0000

    Partical-lines

--- Experimentation/Partical-Line/js/index.js
+++ Experimentation/Partical-Line/js/index.js
@@ -1,4 +1,4 @@
-var renderer, scene, camera, particle;
+var renderer, scene, camera, particle, cubes;
 
 window.onload = function() {
   init();
@@ -30,14 +30,23 @@ function init() {
   scene.add(camera);
 
   particle = new THREE.Object3D();
+  cubes = new THREE.Object3D();
 
   scene.add(particle);
+  scene.add(cubes);
 
   var geometry = new THREE.TetrahedronGeometry(2, 4);
+  var cubeGeometry = new THREE.BoxGeometry(200, 200, 200);
 
   var material = new THREE.MeshPhongMaterial({
-    color: 0xffffff,
+    color: 0xfff600,
     shading: THREE.FlatShading
+  });
+
+  var cubeMaterial = new THREE.MeshPhongMaterial({
+    color: 0x92fcf5,
+    shading: THREE.FlatShading,
+    wireframe: true
   });
 
   for (var i = 0; i < 1000; i++) {
@@ -48,6 +57,15 @@ function init() {
     particle.add(mesh);
   }
 
+  for (var i = 0; i < 10; i++) {
+    var meshCube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    meshCube.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
+    meshCube.position.multiplyScalar(0.2 + (Math.random() * 40));
+    meshCube.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
+
+    cubes.add(meshCube);
+  }
+
   var ambientLight = new THREE.AmbientLight(0x70a6ff );
   scene.add(ambientLight);
 
@@ -56,7 +74,7 @@ lights[0] = new THREE.DirectionalLight( 0xffffff, 1 );
 lights[0].position.set( 1, 0, 0 );
 lights[1] = new THREE.DirectionalLight( 0x70a6ff, 1 );
 lights[1].position.set( 0.75, 1, 0.5 );
-lights[2] = new THREE.DirectionalLight( 0x22f433, 1 );
+lights[2] = new THREE.DirectionalLight( 0xf46f4e, 1 );
 lights[2].position.set( -0.75, -1, 0.5 );
 scene.add( lights[0] );
 scene.add( lights[1] );
@@ -70,6 +88,8 @@ function animate() {
 
   particle.rotation.x += 0.0000;
   particle.rotation.y -= 0.0020;
+  cubes.rotation.x += 0.0000;
+  cubes.rotation.y -= 0.0020;
 
 
   renderer.render( scene, camera )
