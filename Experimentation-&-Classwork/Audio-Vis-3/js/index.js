f417e419114d336939f579465b92dd305925ac00
ThomasKerslake <33869524+ThomasKerslake@users.noreply.github.com>
2018-11-29 00:16:47 +0000

    Design edit / bug fix(s)

--- Experimentation-&-Classwork/Audio-Vis-3/js/index.js
+++ Experimentation-&-Classwork/Audio-Vis-3/js/index.js
@@ -4,30 +4,33 @@ class Visualization{
         //Changeable options
         this.particles = options.particles;
         this.ease = options.ease;
-        //particle storage
+        //Particle storage
         this.particlesStored = [];
+        //Stores new objs
         this.myObj = [];
-        //This is a
+        //The starting value of scences that need to be subtracted from length of scene.children.length
+        //It is used to stop the value of the for loop in the render from overflowing
+        //For every new addition to my scene the value increments by (1) --> this.segregation++
         this.segregation = 1;
     }
 
+//Start of the Init
     init(){
-        //Start of the Init
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 5000 );
         this.renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
         this.renderer.setClearColor( 0x000000, 0 );
+        this.renderer.setClearColor(0x000000, 0.0);
         this.camera.position.set(0, -95, 50);
-        this.windowHeight = $(window).height();
-        this.windowWidth = $(window).width();
+        this.windowHeight = window.innerHeight;
+        this.windowWidth = window.innerWidth;
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.autoClear = false;
-        this.renderer.setClearColor(0x000000, 0.0);
         this.renderer.shadowMap.enabled = true;
 				this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         this.index = 0;
-        this.shape = ['clam','speaker','ring','line'];
-        this.octahedron = ['oct1']
+        this.shape = ['ring'];
+        this.octahedron = ['oct1','octc'];
 
         //Setting up the Orbit Controls
         this.controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
@@ -37,16 +40,16 @@ class Visualization{
 
         this.render();
         this.addParticles();
-        //this.addLight();
         this.addAmbientLight();
         this.addCenter();
         this.addPlane();
         this.addSpotLight();
+        this.addCenterChildren();
+        this.initAudio();
 
         //Add listeners
-        $(window).on('resize', this.resize.bind(this));
-        $('input').on('change', this.inputChange.bind(this));
-        this.initAudio();
+        window.addEventListener( 'resize', this.resize.bind(this));
+        document.getElementById("file").addEventListener('change', this.onFileSelect.bind(this));
 
 			//End of INIT -----------------------------------------------------------
     }
@@ -72,49 +75,38 @@ class Visualization{
         this.analyser.getByteFrequencyData(this.frequencyData);
     }
 
-    inputChange(e){
+    onFileSelect(z){
         //Links the selected audio (taget file -> posistion [0])
-        this.audioObj.src = URL.createObjectURL(e.target.files[0]);
+        this.audioObj.src = URL.createObjectURL(z.target.files[0]);
         this.audioObj.play();
         this.analyseAudio();
-        //Removes 'click to add MP3' on change
-        $('input, label').remove();
+        //Removes 'text' on change
+        document.getElementById("myLabel").outerHTML = "";
+        document.getElementById("file").outerHTML = "";
     }
 
     render(){
         requestAnimationFrame(this.render.bind(this));
         for (let i=0; i<(this.scene.children.length)-this.segregation; i++) {
             //Choose Shape to render
-            this[this.shape[2]](this.particlesStored[i],i, this.frequencyData[i]);
-            this[this.octahedron[0]](this.myObj[0])//this.frequencyData[i]
+            this[this.shape[0]](this.particlesStored[i],i, this.frequencyData[i]);
+            this[this.octahedron[0]](this.myObj[0]);
+            this[this.octahedron[1]](this.myObj[1],this.frequencyData[i]);//this.frequencyData[i]
+            this[this.octahedron[1]](this.myObj[2],this.frequencyData[i]);
+            this[this.octahedron[1]](this.myObj[3],this.frequencyData[i]);
+            this[this.octahedron[1]](this.myObj[4],this.frequencyData[i]);
+            this[this.octahedron[0]](this.myObj[5]);
         }
-        //Render
-        //console.log(this.scene.children.length);
         this.renderer.render( this.scene, this.camera );
     }
 
     resize(){
-        this.windowHeight = $(window).height();
-        this.windowWidth = $(window).width();
+        this.windowHeight = window.innerHeight;
+        this.windowWidth = window.innerWidth;
         this.camera.aspect = this.windowWidth / this.windowHeight;
         this.camera.updateProjectionMatrix();
         this.renderer.setSize(this.windowWidth, this.windowHeight);
     }
-
-    /*addLight(){
-      var lights = [];
-      lights[0] = new THREE.DirectionalLight( 0xffffff, 1 );
-      lights[0].position.set( 1, 0, 0 );
-      lights[1] = new THREE.DirectionalLight( 0xffffff, 1 );
-      lights[1].position.set( 0.75, 1, 0.5 );
-      lights[2] = new THREE.DirectionalLight( 0xffffff, 1 );
-      lights[2].position.set( -0.75, -1, 0.5 );
-      this.scene.add( lights[0] );
-      this.segregation++
-      this.scene.add( lights[1] );
-      this.segregation++
-      this.scene.add( lights[2] );
-    }*/
 
     addAmbientLight(){
       let ambLight
@@ -125,18 +117,15 @@ class Visualization{
     addSpotLight(){
       var spotLight = new THREE.SpotLight( 0xc4c4c4 );
       spotLight.position.set( 0, 0, 150 );
-
       spotLight.castShadow = true;
-
       spotLight.shadow.mapSize.width = 1024;
       spotLight.shadow.mapSize.height = 1024;
-
       spotLight.shadow.camera.near = 10;
       spotLight.shadow.camera.far = 1000;
       spotLight.shadow.camera.fov = 90;
 
-       this.scene.add( spotLight );
-       this.segregation++
+      this.scene.add( spotLight );
+      this.segregation++
      }
 
     addCenter(){
@@ -146,16 +135,65 @@ class Visualization{
        color:0x8c1dd1,
       });
      myObj = new THREE.Mesh(this.centerObj, this.centerObjMat);
-     myObj.position.z = 30;
+     myObj.position.z = 35;
      myObj.castShadow = true;
      this.scene.add(myObj);
      this.myObj.push(myObj);
      this.segregation++
     }
 
+    addCenterChildren(){
+      let myChildObj1, myChildObj2, myChildObj3, myChildObj4, myChildObj5;
+      this.childObj = new THREE.OctahedronGeometry(7,0);
+      this.childObjMat = new THREE.MeshLambertMaterial({
+       color:0x8c1dd1,
+      });
+     myChildObj1 = new THREE.Mesh(this.childObj, this.childObjMat);
+     myChildObj1.position.z = 20;
+     myChildObj1.position.y = 30;
+     myChildObj1.castShadow = true;
+     this.scene.add(myChildObj1);
+     this.myObj.push(myChildObj1);
+     this.segregation++
+
+     myChildObj2 = new THREE.Mesh(this.childObj, this.childObjMat);
+     myChildObj2.position.z = 20;
+     myChildObj2.position.x = -30;
+     myChildObj2.castShadow = true;
+     this.scene.add(myChildObj2);
+     this.myObj.push(myChildObj2);
+     this.segregation++
+
+     myChildObj3 = new THREE.Mesh(this.childObj, this.childObjMat);
+     myChildObj3.position.z = 20;
+     myChildObj3.position.x = 30;
+     myChildObj3.castShadow = true;
+     this.scene.add(myChildObj3);
+     this.myObj.push(myChildObj3);
+     this.segregation++
+
+     myChildObj4 = new THREE.Mesh(this.childObj, this.childObjMat);
+     myChildObj4.position.z = 20;
+     myChildObj4.position.y = -30;
+     myChildObj4.castShadow = true;
+     this.scene.add(myChildObj4);
+     this.myObj.push(myChildObj4);
+     this.segregation++
+
+     myChildObj5 = new THREE.Mesh(this.childObj, this.childObjMat);
+     myChildObj5.position.z = 60;
+     myChildObj5.scale.z = 0.9;
+     myChildObj5.scale.x = 0.5;
+     myChildObj5.scale.y = 0.5;
+     myChildObj5.castShadow = true;
+     this.scene.add(myChildObj5);
+     this.myObj.push(myChildObj5);
+     this.segregation++
+    }
+
     addPlane(){
       let myPlane;
-      this.planeGeo = new THREE.PlaneGeometry(350, 350, 32);
+      this.planeGeo = new THREE.PlaneGeometry(300, 300, 32);
       this.planeMat = new THREE.MeshLambertMaterial( {color: 0xff5858, side: THREE.FrontSide} );
       myPlane = new THREE.Mesh( this.planeGeo, this.planeMat );
       myPlane.receiveShadow = true;
@@ -185,47 +223,32 @@ class Visualization{
       //part.scale.y += ((frequency * this.ease * 0.2) - part.scale.y) * this.ease;
       //part.scale.x += ((frequency * this.ease * 0.2) - part.scale.x) * this.ease;
       part.rotation.z += 0.00002;
+
+    }
+
+    octc(part, frequency){
+      part.position.z += ((frequency * this.ease * 1) - part.position.z) * this.ease;
+      part.rotation.z += (frequency * this.ease * 0.0002) * this.ease;
+      part.scale.z = 1;
+      part.scale.x = 0.7;
+      part.scale.y = 0.7;
+      part.position.z = 5;
     }
 
     // These are different shapes that can be used when creating the layout of the particals
     //Change value of this.shape
 
-	ring(part, index,frequency){
+	ring(part, index, frequency){
     var v = (index + 1000 + frequency * 0.5) * 0.05;
    part.position.x += ((Math.sin(index * 100) * v) - part.position.x) * this.ease;
    part.position.y += ((Math.cos(index * 100) * v) - part.position.y) * this.ease;
    part.position.z += ((frequency * this.ease * 2) - part.position.z) * this.ease;
    part.rotation.z -= 0.02;
     }
-
-    //Line
-    line(part,index,frequency){
-        let sqrt = Math.sqrt(this.scene.children.length);
-      part.position.x += ((index % sqrt) * 10 - sqrt)  - part.position.x;
-      part.position.y += ((Math.floor(index /1000) * 10 - sqrt) - part.position.y);
-      part.position.z += ((frequency * 0.5) - part.position.z) * this.ease;
-     }
-
-    //Speaker
-  speaker(part, index,frequency){
-        var v = (index + 100 + frequency * 0.5) * 0.2;
-      part.position.x += ((Math.sin(index * -0.2) * v) - part.position.x) * this.ease;
-      part.position.y += ((Math.cos(index * -0.2) * v) - part.position.y) * this.ease;
-      part.position.z += ((frequency * this.ease * 3.5) - part.position.z) * this.ease;
-      }
-
-    //Clam
-     clam(c,index,frequency){
-       var v = (index + 100 + frequency * 0.5) * 0.2;
-      c.position.x += ((Math.sin(index * 100) * v) - c.position.x) * this.ease;
-      c.position.y += ((Math.cos(index * 100) * v) - c.position.y) * this.ease;
-      c.position.z += ((frequency * this.ease * 3) - c.position.z) * this.ease;
-      }
-
 }
 
 //On load run class
-$(function(){
+window.onload = ()=>{
     var animate = new Visualization({particles: 150, ease: 0.15});
     animate.init();
-});
+};
